f0606e49b053f056598d2e1bbfcf876c125f92d9
brandonLee <brandonlee196582@gmail.com>
2023-02-27 14:48:49 -0700

    added url fetch that creates buttons from results

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ToggleButton } from './toggleButton.js';
 
 export const App = () => {
   const [messages, setMessages] = useState(['message1','message2','message3','message4']);
+  const [test, setTest] = useState("");
+  const [cardList, setCardList] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.scryfall.com/cards/search?q=forest`)
+    .then(response => response.json())
+    .then(searchData => {
+      setCardList(searchData.data.map(card => {
+        return {cardName: card.name, picUrl: card.image_uris.small}
+      })
+      )
+    })
+}, [test])
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          {messages.map(message => <ToggleButton toggleText={message} />)}
+          {cardList.map(card => <ToggleButton toggleText={card.cardName} toggleImage={card.picUrl} />)}
         </div>
       </header>
     </div>
